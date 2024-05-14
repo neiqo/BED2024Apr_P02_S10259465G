5864e8473aeccb640421e821f8821b445aebd409
neil <zaquadq@gmail.com>
2024-05-15 01:25:30 +0800

    Week 5 Practical Complete!

---
+++ Week5/books-api-mvc-db/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const sql = require("mssql");
+const dbConfig = require("./dbConfig");
+const booksController = require("./controllers/booksController");
+const bodyParser = require("body-parser"); // Import body-parser
+const validateBook = require("./middlewares/validateBook");
+
+
+const app = express();
+const staticMiddleware = express.static("public");
+const port = process.env.PORT || 3000; // Use environment variable or default port
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+app.use(staticMiddleware); // Mount the static middleware
+
+app.get("/books", booksController.getAllBooks);
+app.get("/books/:id", booksController.getBookById);
+app.post("/books", validateBook, booksController.createBook); // POST for creating books (can handle JSON data)
+app.put("/books/:id", validateBook, booksController.updateBook);
+app.delete("/books/:id", booksController.deleteBook); // DELETE for deleting books
+
+
+app.listen(port, async () => {
+  try {
+    // Connect to the database
+    await sql.connect(dbConfig);
+    console.log("Database connection established successfully");
+  } catch (err) {
+    console.error("Database connection error:", err);
+    // Terminate the application with an error code (optional)
+    process.exit(1); // Exit with code 1 indicating an error
+  }
+
+  console.log(`Server listening on port ${port}`);
+});
+
+// Close the connection pool on SIGINT signal
+process.on("SIGINT", async () => {
+  console.log("Server is gracefully shutting down");
+  // Perform cleanup tasks (e.g., close database connections)
+  await sql.close();
+  console.log("Database connection closed");
+  process.exit(0); // Exit with code 0 indicating successful shutdown
+});
